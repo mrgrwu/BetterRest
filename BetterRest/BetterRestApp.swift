953e4e036583a78c6229ddc3f18eea6cb55a1e6b
Greg Wu <mrgrwu@gmail.com>
2021-09-22 13:42:41 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Greg Wu on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
